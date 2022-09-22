ad777a305e7e0584afe6389f6f5fa890ccdd1e4a
WEBDESIGN-PC6\WEBDESIGN <dnckd942522@gmail.com>
2022-09-22 20:47:17 +0900

    no message

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,26 @@
 import Link from "next/link";
 import { FaInstagram, FaGithub } from "react-icons/fa";
 import HeaderStyle from "../styles/HeaderStyle";
+import { useState } from "react";
 
 const Header = () => {
-    return (
-      <header className="position-absolute w-100 d-flex pl-5 pr-5 pt-3 pb-3 justify-content-between align-items-center">
-        
-        <style jsx>{HeaderStyle}</style>
-      </header>
-    );
-}
+  return (
+    <header className="position-absolute w-100 d-flex pl-5 pr-5 pt-3 pb-3 justify-content-between align-items-center">
+      <div className="logo">
+        <Link href="/">
+          <a>
+            <h2>
+              WOOCHANG<b>.</b>
+            </h2>
+          </a>
+        </Link>
+      </div>
+      <div className="hamburger d-flex  flex-column justify-content-center position-relative align-items-end">
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <style jsx>{HeaderStyle}</style>
+    </header>
+  );
+};
 export default Header;
--- styles/HeaderStyle.jsx
+++ styles/HeaderStyle.jsx
@@ -3,5 +3,30 @@ const HeaderStyle = css`
   header {
     color: #fff;
   }
+
+  header .logo h2 {
+    font-family: "noto-bold";
+  }
+  header .logo h2 b {
+    color: var(--point-orange);
+  }
+  header .hamburger {
+    cursor: pointer;
+  }
+
+  header .hamburger .bar {
+    width: 50px;
+    height: 5px;
+    background-color: #fff;
+    border-radius: 100px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+  }
+
+  header .hamburger .bar:nth-child(3) {
+    width: 30px;
+    transition: 0.2s;
+  }
+    
 `;
 export default HeaderStyle;
