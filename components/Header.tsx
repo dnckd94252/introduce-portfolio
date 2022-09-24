47559fd2b4daf1c9027b2a2893c0cfb11b44df29
WEBDESIGN-PC6\WEBDESIGN <dnckd942522@gmail.com>
2022-09-24 16:34:41 +0900

    no message

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,103 @@
 import Link from "next/link";
-import { FaInstagram, FaGithub } from "react-icons/fa";
+import { MdOutlineCircle } from "react-icons/md";
+import { BsInstagram, BsGithub } from "react-icons/bs";
 import HeaderStyle from "../styles/HeaderStyle";
 import { useState } from "react";
 
 const Header = () => {
-  const [open , setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const openAction = () => {
     const val = !open ? true : false;
-    setOpen(val); 
-  }
+    setOpen(val);
+  };
 
   return (
-    <header className="position-absolute w-100 d-flex pl-5 pr-5 pt-3 pb-3 justify-content-between align-items-center">
-      <div className="logo">
-        <Link href="/">
-          <a>
-            <h2>
-              WOOCHANG<b>.</b>
-            </h2>
-          </a>
-        </Link>
+    <header className={`position-absolute w-100 pt-3 pb-3 ${!open ? null : 'active'}`}>
+      <div className="menu w-100 d-flex  pl-5 pr-5 align-items-center justify-content-between">
+        <div className="logo">
+          <Link href="/">
+            <a>
+              <h2>
+                WOOCHANG<b>.</b>
+              </h2>
+            </a>
+          </Link>
+        </div>
+        <div
+          className={`hamburger d-flex flex-column justify-content-center position-relative align-items-end ${
+            !open ? null : "active"
+          }`}
+          onClick={openAction}
+        >
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
       </div>
-      <div className={`hamburger d-flex flex-column justify-content-center position-relative align-items-end ${!open ? null : 'active'}`} onClick={openAction}>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
+      <div className="wrapper d-flex flex-column">
+        <div className="content w-100 align-items-center justify-content-between d-flex ">
+          <div className="backMenu  align-items-center d-flex pl-5">
+            <h1>
+              MENU <br /> <div className="bar"></div>
+            </h1>
+          </div>
+          <div className="d-flex flex-column align-items-end items pb-5 mb-5">
+            <Link href="/" className="menuItem">
+              <a>
+                <div className="d-flex align-items-center item justify-content-between">
+                  <div className="decoration d-flex align-items-center">
+                    <MdOutlineCircle />
+                    <div className="decoBar"></div>
+                  </div>
+                  <span>HOME</span>
+                </div>
+              </a>
+            </Link>
+            <Link href="/" className="menuItem">
+              <div className="d-flex align-items-center item justify-content-between">
+                <div className="decoration d-flex align-items-center">
+                  <MdOutlineCircle />
+                  <div className="decoBar"></div>
+                </div>
+                <span>CONTACT</span>
+              </div>
+            </Link>
+            <Link href="/" className="menuItem">
+              <a>
+                <div className="d-flex align-items-center item justify-content-between">
+                  <div className="decoration d-flex align-items-center">
+                    <MdOutlineCircle />
+                    <div className="decoBar"></div>
+                  </div>
+                  <span>ABOUT</span>
+                </div>
+              </a>
+            </Link>
+            <Link href="/" className="menuItem">
+              <a>
+                <div className="d-flex align-items-center item justify-content-between">
+                  <div className="decoration d-flex align-items-center">
+                    <MdOutlineCircle />
+                    <div className="decoBar"></div>
+                  </div>
+                  <span>WORK</span>
+                </div>
+              </a>
+            </Link>
+          </div>
+        </div>
+        <div className="menu-footer pl-5 pr-5 d-flex align-items-center justify-content-between ">
+          <div className="d-flex comment justify-content-center flex-column">
+            <span>Are You</span>
+            <span>Need Me</span>
+            <span>?</span>
+          </div>
+          <div className="d-flex align-items-center justify-content-between sns">
+            <BsGithub />
+            <BsInstagram />
+          </div>
+        </div>
       </div>
       <style jsx>{HeaderStyle}</style>
     </header>
--- styles/HeaderStyle.jsx
+++ styles/HeaderStyle.jsx
@@ -2,8 +2,18 @@ import css from "styled-jsx/css";
 const HeaderStyle = css`
   header {
     color: #fff;
+    z-index: 100;
+    overflow-y: hidden;
+    height: 100px;
+    transition: 0.5s;
   }
-
+  header.active {
+    height: 100vh;
+    border-bottom: 1px solid #fff;
+  }
+  header .logo {
+    z-index: 100;
+  }
   header .logo h2 {
     font-family: "noto-bold";
   }
@@ -37,6 +47,139 @@ const HeaderStyle = css`
   header .hamburger.active .bar:nth-child(3) {
     transform: rotate(-45deg);
     position: absolute;
+  }
+
+  header .wrapper {
+    height: 100vh;
+    overflow: hidden;
+    background-color: #131313;
+  }
+
+  header .wrapper h1,
+  header .wrapper .item {
+    opacity: 0;
+  }
+  header .content {
+    height: 760px;
+  }
+
+  header .content .items {
+    margin-right: 300px;
+  }
+  header .content .items span {
+    font-size: 70px;
+    font-family: "noto-black";
+    margin-top: 15px;
+    margin-bottom: 15px;
+  }
+
+  header .content .items .item {
+    width: 560px;
+    position: relative;
+  }
+  header .content .decoration .decoBar {
+    position: absolute;
+    height: 1px;
+    left: 20px;
+    border-radius: 100px;
+    background-color: #fff;
+  }
+
+  @keyframes decoBarWidth {
+    from {
+      width: 0px;
+    }
+    to {
+      width: 200px;
+    }
+  }
+
+  header .content .items .item:hover .decoBar {
+    animation: decoBarWidth 0.3s forwards;
+  }
+
+  header .content .backMenu h1 {
+    font-size: 430px;
+    position: absolute;
+    font-family: "noto-black";
+    letter-spacing: -30px;
+    padding-bottom: 200px;
+    color: #000;
+  }
+  header .content .backMenu .bar {
+    width: 400px;
+    background-color: #000;
+    height: 50px;
+    margin-left: 40px;
+  }
+
+  header .menu-footer {
+    height: 100px;
+  }
+
+  header .menu-footer .sns {
+    font-size: 40px;
+    width: 100px;
+  }
+
+  header .menu-footer .comment {
+    font-family: "noto-black";
+    color: #222;
+    line-height: 25px;
+    letter-spacing: -1px;
+    font-size: 25px;
+  }
+
+  header.active .wrapper h1 {
+    animation: backMenuActive 0.5s forwards;
+  }
+  header .wrapper h1 {
+    animation: backMenuNotActive 0.5s forwards;
+  }
+  header.active .wrapper .content .items .item{
+    animation: menuActive 0.5s forwards;
+    /* opacity: 1; */
+  } 
+
+  @keyframes backMenuActive {
+    from {
+      opacity: 0;
+      transform: translateX(-100px);
+    }
+    to {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  }
+  @keyframes backMenuNotActive {
+    from {
+      opacity: 1;
+      transform: translateX(0);
+    }
+    to {
+      opacity: 0;
+      transform: translateX(-100px);
+    }
+  }
+  @keyframes menuActive {
+    from {
+      opacity: 0;
+      transform: translateX(100px);
+    }
+    to {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  }
+  @keyframes menuNotActive {
+    from {
+      opacity: 1;
+      transform: translateX(0);
+    }
+    to {
+      opacity: 0;
+      transform: translateX(-100px);
+    }
   }
 `;
 export default HeaderStyle;
