c78c282998b239cfbac930a0d5e48aae813e4eeb
DESKTOP-7OG5567\dnckd <dnckd942522@gmail.com>
2022-09-30 02:59:12 +0900

    work 섹션 css 추가 및 애니메이션 node js작업 들어가기 바람

--- components/main/work/Work.tsx
+++ components/main/work/Work.tsx
@@ -1,5 +1,7 @@
 import WorkStyle from "../../../styles/work/WorkStyle";
 import Image from "next/image";
+import BsArrowRightSquare from "react-icons/bs";
+
 const Work = () => {
   return (
     <section id="work" className="pt-5 pb-5">
@@ -8,8 +10,8 @@ const Work = () => {
           <span>SELECTED PROJECTS</span>
           <h1>Case studies</h1>
         </div>
-        <div className="contents  d-flex align-items-center justify-content-between flex-column">
-          <div className="item position-relative d-flex justify-content-between align-items-baseline">
+        <div className="contents d-flex align-items-center justify-content-between flex-column">
+          <div className="item position-relative d-flex justify-content-between">
             <Image
               src={require("../../../public/contents/4926289.jpg")}
               className="baseImage position-absolute"
@@ -17,8 +19,12 @@ const Work = () => {
               objectPosition="center"
               layout="fill"
             />
-            <div className="">
-              
+            <div className="bg-navy position-absolute w-100 h-100"></div>
+            <div className="circle-wrapper m-5 d-flex align-items-center position-relative justify-content-center">
+              <div className="circle-border position-absolute w-100 h-100"></div>
+              <div className="circle-inner d-flex align-items-center justify-content-center">
+                SOLO
+              </div>
             </div>
             <div className="text d-flex flex-column p-5">
               <span className="year">2022.07 ~ 2022.08</span>
@@ -26,6 +32,7 @@ const Work = () => {
                 GOTDOC Web App Service for Communication of Physicians, Users,
                 and Managers
               </h2>
+              <p>View More About </p>
             </div>
           </div>
         </div>
--- styles/work/WorkStyle.jsx
+++ styles/work/WorkStyle.jsx
@@ -18,6 +18,17 @@ const WorkStyle = css`
     object-fit: cover;
     width: 100%;
     height: 800px;
+    cursor: pointer;
+  }
+
+  #work .contents .item .bg-navy {
+    background-color: #0e1322;
+    opacity: 0;
+    transition: 0.3s;
+  }
+
+  #work .contents .item:hover .bg-navy {
+    opacity: 0.95;
   }
 
   #work .contents .item .text {
@@ -37,6 +48,53 @@ const WorkStyle = css`
     font-size: 30px;
     letter-spacing: -1px;
     word-break: keep-all;
+  }
+
+  #work .contents .item .circle-wrapper {
+    z-index: 3;
+    width: 80px;
+    height: 80px;
+    overflow: hidden;
+    border-radius: 100%;
+    transition: 0.2s;
+  }
+
+  #work .contents .item .circle-wrapper .circle-border {
+    background-color: #fff;
+    transition: 0.2s;
+  }
+
+  #work .contents .item .circle-inner {
+    font-family: "noto-black";
+    background-color: #121212;
+    width: 70px;
+    height: 70px;
+    border-radius: 100%;
+    z-index: 4;
+    color: #fff;
+  }
+
+  #work .contents .item:hover .text h2 {
+    color: #fff;
+    transition: 0.3s;
+    animation: up-text-h2 0.5s .2s forwards ;
+  }
+
+  #work .contents .item:hover .text span {
+    color: #ddd;
+    transition: 0.3s;
+    animation: up-text-h2 0.5s forwards;
+  }
+
+  @keyframes up-text-h2 {
+    0% {
+      opacity: 0;
+      transform: translateY(30px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
   }
 `;
 export default WorkStyle;
