0064a264044308e61fb19ec742c83c25e0bb3451
DESKTOP-7OG5567\dnckd <dnckd942522@gmail.com>
2022-10-20 02:26:57 +0900

    no message

--- server/controller/WorkController.js
+++ server/controller/WorkController.js
@@ -3,7 +3,8 @@ const mysql = require("../mysql");
 class WorkController {
   async create(req, res) {
     const { files, body } = req;
-    const { name, startMonth, endMonth, type, content, stack, role } = body;
+    const { url, name, startMonth, endMonth, type, content, stack, role } =
+      body;
 
     const mockup = req.files
       .filter(item => item.fieldname === "mockup")
@@ -12,21 +13,56 @@ class WorkController {
     const image = req.files
       .filter(item => item.fieldname === "image")
       .map(item => item.filename);
-    
+
+    const monthToDateFormat = month => {
+      const monthDate = new Date(month);
+      const monthVal = monthDate.getMonth() + 1;
+      const yearVal = monthDate.getFullYear();
+      const dateVal = monthDate.getDate();
+      return `${yearVal}-${monthVal}-${dateVal}`;
+    };
+
     const data = {
       name,
-      startMonth,
-      endMonth,
+      startMonth: monthToDateFormat(startMonth),
+      endMonth: monthToDateFormat(endMonth),
       type,
       content,
       stack: stack.length > 1 ? stack : [stack],
       role: role.length > 1 ? role : [role],
       mockup,
-      image
+      image,
+      url,
     };
 
-    
-}
+    console.log(data.endMonth);
+
+    const sql = {
+      workInsert:
+        " INSERT INTO works(name , start_date , end_date , type , mockup , content , url) VALUES (?,?,?,?,?,?,?)",
+      workStackInsert: "INSERT INTO work_stacks(work_id , stack) VALUES (?,?)",
+      workRoleInsert: "INSERT INTO work_roles(work_id , role) VALUES (?,?)",
+      workImageInsert:
+        "INSERT INTO work_images(src , work_id , name) VALUES (?,?,?)",
+    };
+
+    const sqlData = {
+      workInsert: [
+        data.name,
+        data.startMonth,
+        data.endMonth,
+        data.type,
+        ...data.mockup,
+        data.content,
+        data.url,
+      ],
+    };
+
+    await mysql.query(sql.workInsert , sqlData.workInsert);
+    for (const element of data.stack) {
+      if (!element || element.length <= 0) continue;
+    }
+  }
 }
 
 module.exports = new WorkController();
